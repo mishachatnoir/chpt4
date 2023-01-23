c50fa9b4f5ff949d5783d91df3248e32320c7772
MishaChatNoir <misha@DESKTOP-NICPFBD.localdomain>
2023-01-23 17:54:23 +0300

    apocalypse

--- prod/services/main.tf
+++ prod/services/main.tf
@@ -1,11 +1,11 @@
 module "webserver-cluster" {
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webserver-stage"
+  cluster_name = "webserver-prod"
   db_remote_state_bucket = "terraform-up-and-running-state-misha"
-  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
 
-  instance_type = "t2.micro"
+  instance_type = "m4.large"
   min_size = 2
   max_size = 10
 }
